d6c137283260dddf2608218c0619aaa005757b86
Alvaro <santunado@gmail.com>
2024-07-14 14:10:49 -0300

    Test storage sol

---
+++ ignition/modules/Storage.js
@@ -0,0 +1,9 @@
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+
+module.exports = buildModule("StorageModule", (m) => {
+
+  const storage = m.contract("Storage");
+
+  return { storage };
+});
